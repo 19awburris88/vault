64e0aa3c046d8bcb58ec365b22f47ad8f9770de2
Austin Burris <19awburris88@gmail.com>
2025-02-18 11:53:17 -0600

    main

---
+++ script.js
@@ -0,0 +1,7 @@
+let message = "This is the combination for the vault”";
+
+let num1 = 5 + 5; // Addition to get 10
+let num2 = 8 * 5; // Multiplication to get 40
+let num3 = 50 - 11; // Subtraction to get 39
+
+alert(`TYou have received this message because you have been chosen to open an important vault. Here is the secret combination ${num1} + ${num2} + ${num3}`)
